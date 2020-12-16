282a27fbd3b175cf922f48e3c96fa5bebc03961d
tzmcion <tymsonekjelonek@gmail.com>
2020-12-16 16:31:02 +0100

    s

--- serwer02.js
+++ serwer02.js
@@ -228,7 +228,7 @@ function HTMLtable_sorted(arrays, boolup) {
 
 var express = require("express")
 var app = express()
-const PORT = 3000;
+var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 var path = require("path")
 var bodyParser = require("body-parser")
